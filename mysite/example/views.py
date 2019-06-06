5b1010b91307027d6507ca00b9d2d1a245f46a68
IavorNikolaev <36873748+IavorNikolaev@users.noreply.github.com>
2019-06-06 12:05:48 +1000

    sTARTED cLASS

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -9,7 +9,7 @@ from .models import *
 def example_get(request, var_a, var_b):
 	try:
 		returnob = {
-		"data": "%s: %s" %(var_a, var_b),
+		"data": "%sverystrongpassword: %spassthedoof" %(var_a, var_b),
 		}
 		return JsonResponse(returnob)
 	except Exception as e:
