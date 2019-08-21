b54f464954d903d282ba4d456a29b0151a4318e6
IavorNikolaev <36873748+IavorNikolaev@users.noreply.github.com>
2019-08-21 15:29:28 +1000

    upload

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -3,6 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import json
 from .models import *
+import os, sys
+import math 
+
 
 # Create your views here.
 
@@ -26,7 +29,92 @@ def example_post(request):
 	if request.method == "POST":
 		try:
 			data = request.POST["data"]
-			return JsonResponse({"log":log})
+			jsob = json.loads(data)
+			print(jsob)
+			print(type(jsob))
+			square = 0 
+
+			sqaured = int(jsob['i1']) * int(jsob['i1'])
+			cubed = int(jsob['i1']) * int(jsob['i1'])
+			cubed = cubed * int(jsob['i1'])
+
+			return JsonResponse({"number squared":squared})
+
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+	else:
+		return HttpResponse("ONLY POST REQUESTS")
+
+
+
+@csrf_exempt
+def fib(request):
+	jsob = {'startnumber': 0, 'length': 10}
+	log = []
+	if request.method == "POST":
+		try:
+			data = request.POST["data"]
+			received = json.loads(data)
+			jsob.update(received)
+
+
+
+			startnumber = int(jsob['startnumber'])
+			length = int(jsob['length'])
+			loop = range(length)
+			numlist = []
+
+			fibnum = startnumber
+			addnum = 1
+
+			for l in loop:
+				numlist.append(fibnum)
+				fibnum = fibnum + addnum
+				addnum = fibnum - addnum 
+
+			return JsonResponse({"fib":numlist})
+
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+	else:
+		return HttpResponse("ONLY POST REQUESTS")
+
+
+
+
+
+@csrf_exempt
+def pythag(request):
+	jsob = {'sideone': 3, 'sidetwo': 5}
+	log = []
+	if request.method == "POST":
+		try:
+			data = request.POST["data"]
+			received = json.loads(data)
+			jsob.update(received)
+
+
+
+			sideone = int(jsob['sideone'])
+			sidetwo = int(jsob['sidetwo'])
+
+
+			side_one_squared = sideone*sideone
+			side_two_squared = sidetwo*sidetwo
+			sides_added = side_one_squared+side_two_squared
+			sidethree = math.sqrt(sides_added)
+			 
+
+			return JsonResponse({"sidethree":sidethree})
+
 		except Exception as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			other = sys.exc_info()[0].__name__
